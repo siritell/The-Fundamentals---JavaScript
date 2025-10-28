7cb1824231559ccd20839396d6237fa072153dea
Siri <siri@example.com>
2025-10-28 14:04:23 +0100

    inital commit

---
+++ main.js
@@ -0,0 +1,17 @@
+import './style.css'
+
+const container = document.getElementById('app');
+
+function createImage(src) {
+  const image = document.createElement('img');
+  image.src = src;
+  container.appendChild(image);
+}
+
+function init() {
+  fetch('https://image-feed-api.vercel.app/api/images/e8cd3ffd-794c-4ec6-b375-7788dbb14275')
+  .then(resp => resp.json())
+  .then(json => createImage(json.image_url))
+}
+
+init();
