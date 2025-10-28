666bc035be71e3655069c49862deeddd169c9edc
Siri <siri@example.com>
2025-10-28 15:11:32 +0100

    updated js

--- main.js
+++ main.js
@@ -1,15 +1,26 @@
 const container = document.getElementById('app');
 
 function createImage(src) {
-  const image = document.createElement('img');
-  image.src = src;
-  container.appendChild(image);
+    const image = document.createElement('img');
+    image.src = src;
+    container.appendChild(image);
 }
 
-function init() {
-  fetch('https://image-feed-api.vercel.app/api/images/e8cd3ffd-794c-4ec6-b375-7788dbb14275')
-  .then(resp => resp.json())
-  .then(json => createImage(json.image_url))
+// function init() {
+//     fetch('https://image-feed-api.vercel.app/api/images/e8cd3ffd-794c-4ec6-b375-7788dbb14275')
+//     .then(resp => resp.json())
+//     .then(json => createImage(json.image_url))
+// }
+
+function getImages() {
+    fetch('https://image-feed-api.vercel.app/api/images')
+    .then(resp => resp.json())
+    .then(json => createImage(json.image_url))
+    console.log();
 }
 
-init();
+getImages();
+
+// function likeImage() {
+//     return { "success": true, "likes_count": 6 }
+// }
