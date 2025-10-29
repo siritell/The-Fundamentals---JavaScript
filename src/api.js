fa33eac8a9c658ac639ae6e7ef28e1f5a7451546
vickhoff <dvickhoff@gmail.com>
2025-10-29 15:40:36 +0100

    Add api calls

---
+++ src/api.js
@@ -0,0 +1,49 @@
+const baseURL = "https://image-feed-api.vercel.app/api/images"
+
+export const getAllImages = async () => {
+    try {
+        const url = baseURL;
+        const response = await fetch(url);
+        const galleryData = await response.json();
+        
+        return galleryData;
+    } catch {
+        console.log("Couldn't get images");
+    }
+}
+
+export const getOneImage = async (id) => {
+    try {
+        const url = `${baseURL}/${id}`;
+        const response = await fetch(url);
+        const imageData = await response.json();
+        
+        return imageData;
+    } catch {
+        console.log("Couldn't get images");
+    }
+}
+
+export const postLike = async (id) => {
+    try {
+        const url = `${baseURL}/${id}/like`;
+        const response = await fetch(url);
+        const likeData = await response.json();
+        
+        return likeData;
+    } catch {
+        console.log("Couldn't post like");
+    }
+}
+
+export const postComment = async (id) => {
+    try {
+        const url = `${baseURL}/${id}/comment`;
+        const response = await fetch(url);
+        const commentData = await response.json();
+        
+        return commentData;
+    } catch {
+        console.log("Couldn't post comment");
+    }
+}
