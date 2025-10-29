7a8c200813ecb3fa3861332b2245d6f190e3849e
Laura_Git <laurama1007@gmail.com>
2025-10-29 22:57:26 +0100

    created like and comment button

--- src/main.js
+++ src/main.js
@@ -1,31 +1,47 @@
 import "./style.css";
-import { getAllImages } from "./api.js";
-import { getOneImage } from "./api.js";
+import { getAllImages, postComment, postLike, getOneImage } from "./api.js";
 
 const container = document.getElementById("app");
 
-function createImage(src) {
+function createImage(imageurl, id) {
   const card = document.createElement("div");
   card.classList.add("article-card");
 
   const image = document.createElement("img");
-  image.src = src;
+  image.src = imageurl;
   image.classList.add("article-image");
-
   card.appendChild(image);
+
+  const likeButton = document.createElement("button");
+
+  likeButton.textContent = "Like 👍";
+  likeButton.classList.add("overlay-button");
+  likeButton.addEventListener("click", () => postLike(id)); // which function to call.. which type of event
+  card.appendChild(likeButton);
+
+  const commentButton = document.createElement("button");
+  let commenter_name = "Test";
+  let comment = "Test comment";
+  commentButton.textContent = "Comment";
+  commentButton.classList.add("overlay-button");
+  commentButton.addEventListener("click", () =>
+    postComment(id, commenter_name, comment)
+  );
+  card.appendChild(commentButton);
+
   container.appendChild(card);
 }
 
 async function createImages() {
-  const gallery = await getAllImages()
+  const gallery = await getAllImages();
 
-  console.log(gallery)
+  console.log(gallery);
 
-
-    for (const image of gallery) {
-      createImage(image.image_url); //Create an image element for each image.
-      console.log(image.image_url);
-    }
+  for (const image of gallery) {
+    createImage(image.image_url, image.id);
+    //Create an image element for each image.
+    console.log(image.image_url);
   }
+}
 
- createImages()
+createImages();
