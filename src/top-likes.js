3f7bffe89b293f30491e7e1c334bbd1618f45cde
vickhoff <dvickhoff@gmail.com>
2025-10-31 15:29:42 +0100

    Integrate topLikes function into main.js to enhance image aggregation and update stats

--- src/top-likes.js
+++ src/top-likes.js
@@ -1,18 +1,19 @@
 import { getAllImages, getAllPages } from "./api.js";
 
 //Get all images in one array
-const topLikes = async () => {
+export const topLikes = async () => {
+
+
     // Get total pages
     const totalPages = await getAllPages();
+    let allImages = []
 
     // Loop through all pages and push images to new array
     for (let page = 1; page <= totalPages; page++) {
         const imagesOnPage = await getAllImages(page);
-        
-        imagesOnPage.map((image) => {
-            return newValue;
-          });
+        allImages.push(...imagesOnPage); // Spread all images and push them
     }
+    console.log(allImages)
 
 
 }
